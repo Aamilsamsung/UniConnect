a27ef1cff019808c7a7af2fae180fa63a86bb330
Aamilsamsung <aamil.samsungm56@gmail.com>
2026-09-16 20:38:07 +0530

    feat: connect auth module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './common/prisma.service';
+import { AuthModule } from './auth/auth.module';
 import { CommunitiesModule } from './communities/communities.module';
 
 @Module({
-  imports: [CommunitiesModule],
+  imports: [AuthModule, CommunitiesModule],
   providers: [PrismaService],
   exports: [PrismaService],
 })
